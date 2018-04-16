26186e0c98583257d1ff0a886c5e8edbb3a5101c
Jay Arellano <jaarellano@csumb.edu>
2018-04-16 23:22:32 +0000

    more

--- shopping-cart/database.php
+++ shopping-cart/database.php
@@ -6,6 +6,14 @@
         $password = "";
         $dbname = "shopping"; 
         
+        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+           $host = $url["host"];
+           $db   = substr($url["path"], 1);
+           $user = $url["user"];
+           $pass = $url["pass"];
+        }
+        
         // Create connection
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
