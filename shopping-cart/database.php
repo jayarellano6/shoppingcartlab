9d2091b8bbc5d90b4f010ffafaaebb8bb1136539
Jay Arellano <jaarellano@csumb.edu>
2018-04-16 23:31:02 +0000

    more

--- shopping-cart/database.php
+++ shopping-cart/database.php
@@ -9,9 +9,9 @@
         if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
            $host = $url["host"];
-           $db   = substr($url["path"], 1);
-           $user = $url["user"];
-           $pass = $url["pass"];
+           $dbname   = substr($url["path"], 1);
+           $username = $url["user"];
+           $password = $url["pass"];
         }
         
         // Create connection
